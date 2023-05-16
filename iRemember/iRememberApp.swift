af723b1ec9789683f17f670452d683924d8ada2d
Matheus Viana <matheus_o_viana@hotmail.com>
2023-05-16 13:09:39 -0300

    Initial Commit

---
+++ iRemember/iRememberApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRememberApp.swift
+//  iRemember
+//
+//  Created by Matheus Viana on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct iRememberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
