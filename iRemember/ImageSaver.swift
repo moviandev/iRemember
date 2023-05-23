ad1a1db94301854479266eb871d72aadebe93466
Matheus Viana <matheus_o_viana@hotmail.com>
2023-05-22 22:47:33 -0300

    implement image saver class

--- iRemember/ImageSaver.swift
+++ iRemember/ImageSaver.swift
@@ -3,6 +3,22 @@
 //  iRemember
 //
 //  Created by Matheus Viana on 22/05/23.
-//
 
-import Foundation
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
