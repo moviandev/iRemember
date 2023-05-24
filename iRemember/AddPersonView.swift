a483313eb1a227df38656b4c6dafc93e81620067
Matheus Viana <matheus_o_viana@hotmail.com>
2023-05-24 13:35:11 -0300

    add AddPersonView

---
+++ iRemember/AddPersonView.swift
@@ -0,0 +1,20 @@
+//
+//  AddPersonView.swift
+//  iRemember
+//
+//  Created by Matheus Viana on 24/05/23.
+//
+
+import SwiftUI
+
+struct AddPersonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddPersonView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPersonView()
+    }
+}
